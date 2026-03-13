3ee02bf7d4bfd66a6a45388f0b302a302472575b
SubLim3 <sublim3.js@hotmail.com>
2026-03-13 18:04:36 -0500

    Update readIP.php

--- readIP.php
+++ readIP.php
@@ -1,90 +1,91 @@
 <?php
-require_once("inc.header.php");
 
-/*
- * Get WiFi IP address
- */
+require_once("func.php");
+
 function getWifiIp() {
 
-    $ip = trim(shell_exec("ip -4 addr show wlan0 2>/dev/null | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}' | head -n 1"));
+    $ip = trim(shell_exec("hostname -I | awk '{print $1}'"));
 
     if (!empty($ip)) {
         return $ip;
     }
 
-    $ip = trim(shell_exec("hostname -I 2>/dev/null | awk '{print $1}'"));
+    $ip = trim(shell_exec("ip -4 addr show wlan0 | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}' | head -n 1"));
 
     if (!empty($ip)) {
         return $ip;
     }
 
-    return "No network connection detected";
+    return "Not connected";
 }
 
 $ipAddress = getWifiIp();
 
-/*
- * Trigger Phoniebox to read the IP address aloud
- */
 shell_exec("bash /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=readwifiipoverspeaker >/dev/null 2>&1 &");
 
+include("header.php");
 ?>
 
 <div class="container">
 
-<div class="row">
-<div class="col-lg-12">
+    <h2>SubLim3 JukeBox IP Address</h2>
 
-<div class="panel panel-primary">
+    <p>The jukebox is speaking the IP address out loud.</p>
 
-<div class="panel-heading">
-<h3 class="panel-title">
-<i class='fa fa-volume-up'></i> Read JukeBox IP Address
-</h3>
-</div>
+    <div style="font-size:32px;font-weight:bold;margin-top:20px;">
+        <?php echo htmlspecialchars($ipAddress); ?>
+    </div>
 
-<div class="panel-body">
+    <br><br>
 
-<pre style="font-size:16px">
-
-.
-.
-.
-.    ___      _    _    _       ____     _      _       ___
-.   / __|_  _| |__| |  (_)_ __ |__ /  _ | |_  _| |_____| _ ) _____ __
-.   \__ \ || | '_ \ |__| | '  \ |_ \ | || | || | / / -_) _ \/ _ \ \ /
-.   |___/\_,_|_.__/____|_|_|_|_|___/  \__/ \_,_|_\_\___|___/\___/_\_\
-.
-.
-.
-
-Speaking WiFi IP address through the speaker...
-
---------------------------------------------------
-
-JukeBox IP Address:
-
-<?php echo $ipAddress; ?>
-
-
-You can connect to the JukeBox admin interface using:
-
-http://<?php echo $ipAddress; ?>
-
-
---------------------------------------------------
-
-IP address has been spoken through the speaker.
-
-</pre>
-
-</div>
-</div>
-</div>
-</div>
+    <a class="btn btn-primary" href="index.php">Return to Home</a>
 
 </div>
 
 <?php
-include("inc.footer.php");
+include("footer.php");
 ?>
+<body>
+<div class="container">
+
+<?php include("inc.navigation.php"); ?>
+
+<div class="panel-group">
+  <div class="panel panel-default">
+
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <i class='mdi mdi-update'></i> SubLim3-JukeBox Update
+      </h4>
+    </div>
+
+    <div class="panel-body">
+
+<?php
+
+$repoDir = "/home/pi/SubLim3-JukeBox";
+$cmd = "sudo -u pi bash -c 'cd $repoDir && git pull -q origin main && bash SubLim3-Updates.sh' 2>&1";
+
+exec($cmd, $output, $returnCode);
+
+echo "<pre>";
+foreach ($output as $line) {
+    echo htmlspecialchars($line) . "\n";
+}
+echo "</pre>";
+
+if ($returnCode === 0) {
+    echo "<div class='alert alert-success'>Update completed successfully.</div>";
+} else {
+    echo "<div class='alert alert-danger'>Update failed or completed with errors.</div>";
+}
+
+?>
+
+    </div>
+  </div>
+</div>
+
+</div>
+</body>
+</html>
