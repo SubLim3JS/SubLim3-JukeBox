facfdcf5282b1d3904f10871d3600835438f00fc
SubLim3 <sublim3.js@hotmail.com>
2026-03-18 08:24:03 -0500

    Update inc.navigation.php

--- inc.navigation.php
+++ inc.navigation.php
@@ -1,25 +1,17 @@
 <?php
-// ----------------------------------------------------
-// Card Register Toggle
-// ----------------------------------------------------
 $toggleFile = '/home/pi/RPi-Jukebox-RFID/settings/reg-toggle';
 $cardRegisterEnabled = false;
 
-if (file_exists($toggleFile)) {
-    $lines = file($toggleFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $config = array();
+if (file_exists($toggleFile) && is_readable($toggleFile)) {
+    $contents = file_get_contents($toggleFile);
 
-    foreach ($lines as $line) {
-        if (strpos($line, '=') !== false) {
-            list($key, $value) = explode('=', $line, 2);
-            $config[trim($key)] = trim($value);
+    if ($contents !== false && strpos($contents, 'enabled=1') !== false) {
+        $expires = '';
+
+        if (preg_match('/^expires=(.*)$/m', $contents, $matches)) {
+            $expires = trim($matches[1]);
         }
-    }
 
-    $isEnabled = isset($config['enabled']) && $config['enabled'] === '1';
-    $expires = isset($config['expires']) ? $config['expires'] : '';
-
-    if ($isEnabled) {
         if ($expires === '') {
             $cardRegisterEnabled = true;
         } else {
@@ -67,26 +59,19 @@ if (file_exists($toggleFile)) {
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-      <!-- Main Menu -->
       <ul class="nav navbar-nav">
         <li><a href='index.php' class='mainMenu'><i class='mdi mdi-play-circle'></i> Player</a></li>
 
-        <?php if ($cardRegisterEnabled): ?>
-        <li>
-            <a href="cardRegisterNew.php" class="mainMenu">
-                <i class='mdi mdi-cards-outline'></i> Card ID
-            </a>
-        </li>
-        <?php endif; ?>
+        <?php if ($cardRegisterEnabled) { ?>
+        <li><a href="cardRegisterNew.php" class="mainMenu"><i class='mdi mdi-cards-outline'></i> Card ID</a></li>
+        <?php } ?>
 
         <li><a href='search.php' class='mainMenu'><i class='mdi mdi-magnify'></i> Search</a></li>
         <li><a href='settings.php' class='mainMenu'><i class='mdi mdi-settings'></i> Settings</a></li>
-        <li><a href="#" class="mainMenu">TEST-NAV-123</a></li>
         <li><a href='systemInfo.php' class='mainMenu'><i class='mdi mdi-information-outline'></i> Info</a></li>
         <li><a href='manageFilesFolders.php' class='mainMenu'><i class='mdi mdi-folder-upload'></i> Folders &amp; Files</a></li>
       </ul>
 
-      <!-- Right Menu -->
       <ul class="nav navbar-nav navbar-right">
         <li><a href='index.php?shutdown=true' class='mainMenu'><i class='mdi mdi-power'></i> Shutdown</a></li>
         <li><a href='index.php?reboot=true' class='mainMenu'><i class='mdi mdi-refresh'></i> Reboot</a></li>
