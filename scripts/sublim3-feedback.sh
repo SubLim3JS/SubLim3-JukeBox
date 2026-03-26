c0d2d54602f6c67ab46b2a364d5e9b7af07ff4e4
SubLim3 <sublim3.js@hotmail.com>
2026-03-25 20:18:31 -0500

    Update sublim3-feedback.sh

--- scripts/sublim3-feedback.sh
+++ scripts/sublim3-feedback.sh
@@ -3,18 +3,19 @@
 SOUND_DIR="/home/pi/RPi-Jukebox-RFID/shared/sounds"
 PLAYER="/usr/bin/aplay"
 DEVICE="plughw:CARD=Headphones,DEV=0"
+EVENT="$1"
 
-case "$1" in
-  update)  FILE="$SOUND_DIR/update.wav" ;;
-  success) FILE="$SOUND_DIR/success.wav" ;;
-  error)   FILE="$SOUND_DIR/error.wav" ;;
-  wifi)    FILE="$SOUND_DIR/wifi.wav" ;;
-  rfid)    FILE="$SOUND_DIR/rfid.wav" ;;
+case "$EVENT" in
+  update)    FILE="$SOUND_DIR/update.wav" ;;
+  success)   FILE="$SOUND_DIR/success.wav" ;;
+  error)     FILE="$SOUND_DIR/error.wav" ;;
+  wifi)      FILE="$SOUND_DIR/wifi.wav" ;;
+  rfid)      FILE="$SOUND_DIR/rfid.wav" ;;
+  card|card-scan|scan) FILE="$SOUND_DIR/rfid.wav" ;;
+  import)    FILE="$SOUND_DIR/success.wav" ;;
   *) exit 1 ;;
 esac
 
 [ -f "$FILE" ] || exit 1
-
 "$PLAYER" -D "$DEVICE" "$FILE" >/dev/null 2>&1 &
-
 exit 0
