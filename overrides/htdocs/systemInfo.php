01f345f08b3fef93e17af75a46f0a483d83f822e
SubLim3 <sublim3.js@hotmail.com>
2026-03-27 17:14:55 -0500

    Update systemInfo.php

--- overrides/htdocs/systemInfo.php
+++ overrides/htdocs/systemInfo.php
@@ -15,6 +15,67 @@ html_bootstrap3_createHeader("en","System Info | SubLim3 JukeBox",$conf['base_ur
 <?php
 include("inc.navigation.php");
 
+/**
+ * Get wireless interface name
+ */
+function getWirelessInterface() {
+    $preferred = "wlan0";
+
+    if (is_dir("/sys/class/net/" . $preferred . "/wireless")) {
+        return $preferred;
+    }
+
+    $interfaces = @scandir("/sys/class/net/");
+    if ($interfaces !== false) {
+        foreach ($interfaces as $iface) {
+            if ($iface === "." || $iface === "..") {
+                continue;
+            }
+            if (is_dir("/sys/class/net/" . $iface . "/wireless")) {
+                return $iface;
+            }
+        }
+    }
+
+    return "";
+}
+
+/**
+ * Get WiFi connection details
+ */
+function getWifiDetails() {
+    $iface = getWirelessInterface();
+
+    $result = array(
+        "interface" => $iface,
+        "connected" => false,
+        "ip" => "Not connected",
+        "ssid" => "Unknown"
+    );
+
+    if (empty($iface)) {
+        $result["ip"] = "No wireless interface found";
+        $result["ssid"] = "No wireless interface found";
+        return $result;
+    }
+
+    $operstate = trim(@file_get_contents("/sys/class/net/" . $iface . "/operstate"));
+    $ip = trim(shell_exec("ip -4 addr show " . escapeshellarg($iface) . " | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}' | head -n 1"));
+    $ssid = trim(shell_exec("iwgetid -r " . escapeshellarg($iface) . " 2>/dev/null"));
+
+    if (!empty($ip) && ($operstate === "up" || !empty($ssid))) {
+        $result["connected"] = true;
+        $result["ip"] = $ip;
+        $result["ssid"] = !empty($ssid) ? $ssid : "Connected";
+    } else {
+        $result["connected"] = false;
+        $result["ip"] = "Not connected";
+        $result["ssid"] = "Not connected";
+    }
+
+    return $result;
+}
+
 // get System Information and parse into variables
 $exec = "lsb_release -a";
 if($debug == "true") {
@@ -26,6 +87,13 @@ $description = substr($res[1], strpos($res[1], ":") + 1, strlen($res[1]) - strpo
 $release = substr($res[2], strpos($res[2], ":") + 1, strlen($res[2]) - strpos($res[2], ":"));
 $codename = substr($res[3], strpos($res[3], ":") + 1, strlen($res[3]) - strpos($res[3], ":"));
 $rpi_temperature = explode("=", exec("sudo vcgencmd measure_temp"))[1];
+
+// WiFi details
+$wifi = getWifiDetails();
+$wifi_interface = $wifi["interface"];
+$wifi_connected = $wifi["connected"] ? "Connected" : "Disconnected";
+$wifi_ip = $wifi["ip"];
+$wifi_ssid = $wifi["ssid"];
 
 // check RPis throttling state
 function checkRpiThrottle() {
@@ -86,6 +154,28 @@ $rpi_throttle = checkRpiThrottle();
         <div class="row">
           <label class="col-md-4 control-label" for=""><?php print $lang['infoOsCodename']; ?></label>
           <div class="col-md-6"><?php echo trim($codename); ?></div>
+        </div>
+        <div class="row">
+          <label class="col-md-4 control-label" for="">WiFi Interface</label>
+          <div class="col-md-6"><?php echo !empty($wifi_interface) ? htmlspecialchars($wifi_interface) : "Not found"; ?></div>
+        </div>
+        <div class="row">
+          <label class="col-md-4 control-label" for="">WiFi Status</label>
+          <div class="col-md-6">
+            <?php if ($wifi["connected"]) { ?>
+              <span class="label label-success"><?php echo htmlspecialchars($wifi_connected); ?></span>
+            <?php } else { ?>
+              <span class="label label-danger"><?php echo htmlspecialchars($wifi_connected); ?></span>
+            <?php } ?>
+          </div>
+        </div>
+        <div class="row">
+          <label class="col-md-4 control-label" for="">WiFi SSID</label>
+          <div class="col-md-6"><?php echo htmlspecialchars($wifi_ssid); ?></div>
+        </div>
+        <div class="row">
+          <label class="col-md-4 control-label" for="">IP Address</label>
+          <div class="col-md-6"><?php echo htmlspecialchars($wifi_ip); ?></div>
         </div>
         <div class="row">
           <label class="col-md-4 control-label" for=""><?php print $lang['infoOsThrottle']; ?></label>
