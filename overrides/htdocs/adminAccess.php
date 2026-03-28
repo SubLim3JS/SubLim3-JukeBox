1bdf4d7f6035d734346e4b9fac80581b67985587
SubLim3 <sublim3.js@hotmail.com>
2026-03-28 18:41:57 -0500

    Update adminAccess.php

--- overrides/htdocs/adminAccess.php
+++ overrides/htdocs/adminAccess.php
@@ -279,8 +279,8 @@ include("inc.navigation.php");
             </div>
 
             <div class="col-md-4" style="margin-bottom:15px;">
-              <a href="cardRegisterNew.php" class="btn btn-warning btn-lg btn-block">
-                <i class='mdi mdi-cards-outline'></i> Open Card Register
+              <a href="manageFilesFolders.php" class="btn btn-warning btn-lg btn-block">
+                <i class='mdi mdi-cards-outline'></i> Open Folder Manager
               </a>
             </div>
 
