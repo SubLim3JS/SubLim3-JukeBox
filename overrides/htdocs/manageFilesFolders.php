b975c70c02d8c20a1f14a1dbbfc02a882b15bd73
SubLim3 <sublim3.js@hotmail.com>
2026-04-02 17:42:11 -0500

    Update manageFilesFolders.php

--- overrides/htdocs/manageFilesFolders.php
+++ overrides/htdocs/manageFilesFolders.php
@@ -177,7 +177,7 @@ if ($_POST['ACTION'] == "folderCreateNew") {
 html_bootstrap3_createHeader("en", "Files and Folders | SubLim3 JukeBox", $conf['base_url']);
 
 ?>
-<body>
+<body class="<?php print htmlspecialchars(isset($sublim3ThemeClass) ? $sublim3ThemeClass : 'sublim3-theme-green'); ?>">
 <div class="container">
 
     <?php
