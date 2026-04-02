cfdb6179cdc8cdffc6d2d79002dab00ba1adb9f3
SubLim3 <sublim3.js@hotmail.com>
2026-04-02 17:39:39 -0500

    Update adminAccess.php

--- overrides/htdocs/adminAccess.php
+++ overrides/htdocs/adminAccess.php
@@ -168,7 +168,7 @@ if (
 
 html_bootstrap3_createHeader("en", "Admin Access | SubLim3 JukeBox", $conf['base_url']);
 ?>
-<body>
+<body class="<?php print htmlspecialchars(isset($sublim3ThemeClass) ? $sublim3ThemeClass : 'sublim3-theme-green'); ?>">
   <div class="container">
 
 <?php include("inc.navigation.php"); ?>
