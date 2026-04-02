af17752e253b9bcffc8fac49631d980adcb23f54
SubLim3 <sublim3.js@hotmail.com>
2026-04-02 17:39:13 -0500

    Update index.php

--- overrides/htdocs/index.php
+++ overrides/htdocs/index.php
@@ -48,7 +48,7 @@ html_bootstrap3_createHeader("en","SubLim3 JukeBox",$conf['base_url']);
   color: white;
 }
 </style>
-<body>
+<body class="<?php print htmlspecialchars(isset($sublim3ThemeClass) ? $sublim3ThemeClass : 'sublim3-theme-green'); ?>">
   <div class="container">
 
 <?php
