d82a61342ed52478251a30d4d9dadeb7070f2a38
SubLim3 <sublim3.js@hotmail.com>
2026-04-05 16:45:18 -0500

    Update cardRegisterNew.php

--- overrides/htdocs/cardRegisterNew.php
+++ overrides/htdocs/cardRegisterNew.php
@@ -361,20 +361,6 @@ if (!empty($_FILES['importFileUpload'])) {
 </div>
 <?php } ?>
 
-    <div class="row" style="margin-top:15px;">
-      <div class="col-sm-4">
-        <a href="manageFilesFolders.php"
-           class="btn btn-lg btn-block"
-           style="
-             background:#FFFFFF00 !important;
-             border-color:#FFFFFF !important;
-             color:#FFFFFF !important;
-           ">
-          <i class="mdi mdi-folder-multiple"></i> Read IP Address
-        </a>
-      </div>
-    </div>
-
 <div class="row">
   <div class="col-lg-12">
     <strong><?php print $lang['globalJumpTo']; ?>:</strong>
@@ -393,6 +379,20 @@ if (!empty($_FILES['importFileUpload'])) {
   </div>
 </div>
 <br/>
+
+    <div class="row" style="margin-top:15px;">
+      <div class="col-sm-4">
+        <a href="manageFilesFolders.php"
+           class="btn btn-lg btn-block"
+           style="
+             background:#FFFFFF00 !important;
+             border-color:#FFFFFF !important;
+             color:#FFFFFF !important;
+           ">
+          <i class="mdi mdi-folder-multiple"></i> Read IP Address
+        </a>
+      </div>
+    </div>
 
     <div class="row" style="margin-top:15px;">
       <div class="col-sm-4">
